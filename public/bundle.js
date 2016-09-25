88b547a33dfce5adb465c149976c599e41eeda89
Tim Getz <timgetz@gmail.com>
2016-09-24 23:30:50 -0400

    Added loading message.

--- public/bundle.js
+++ public/bundle.js
@@ -24939,19 +24939,25 @@
 
 	    getInitialState: function getInitialState() {
 	        return {
-	            location: 'Miami',
-	            temp: 88
+	            isLoading: false
 	        };
 	    },
 	    handleSearch: function handleSearch(location) {
 	        var that = this;
 
+	        this.setState({
+	            isLoading: true
+	        });
 	        openWeatherMap.getTemp(location).then(function (temp) {
 	            that.setState({
 	                location: location,
-	                temp: temp
+	                temp: temp,
+	                isLoading: false
 	            });
 	        }, function (errorMessage) {
+	            that.setState({
+	                isLoading: false
+	            });
 	            alert(errorMessage);
 	        });
 	    },
@@ -24959,6 +24965,20 @@
 	        var _state = this.state;
 	        var temp = _state.temp;
 	        var location = _state.location;
+	        var isLoading = _state.isLoading;
+
+
+	        function renderMessage() {
+	            if (isLoading) {
+	                return React.createElement(
+	                    'h3',
+	                    null,
+	                    'Fetching weather...'
+	                );
+	            } else if (temp && location) {
+	                return React.createElement(WeatherMessage, { temp: temp, location: location });
+	            }
+	        }
 
 	        return React.createElement(
 	            'div',
@@ -24973,7 +24993,7 @@
 	                )
 	            ),
 	            React.createElement(WeatherForm, { onSearch: this.handleSearch }),
-	            React.createElement(WeatherMessage, { temp: temp, location: location })
+	            renderMessage()
 	        );
 	    }
 	});
