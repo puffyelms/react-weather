3c8274b3761322f2c9b4b17e0721124a1cafdc05
Tim Getz <timgetz@gmail.com>
2016-09-25 23:21:16 -0400

    Added to about

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
+    console.log('req='+req);
+    console.log('get='+req.get('x-forwarded-proto'));
+    console.log('headers='+req.headers['x-forwarded-proto']);
+    if (typeof req.get('x-forwarded-proto') === 'undefined') {
+        console.log('it is undefined');
+        next();
+    } else if (req.get('x-forwarded-proto') === 'http') {
+        console.log('http so doing next');
         next();
     } else {
+        console.log('https so doing redirect - '+req.get('x-forwarded-proto'));
+        console.log('http://' + req.hostname + req.url);
         res.redirect('http://' + req.hostname + req.url);
     }
 });
